7913c4bf4dcf5f5117a64f2d40e465778e8d9785
Timtam <software@satoprogs.de>
2014-08-10 20:58:39 +0200

    - removed the "src" subdirectory - rewrote the setup.py file to ignore lib files and work correctly with the moved folder - decided to ignore pep8 compatibility, because the whole code isn't compatible...

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from distutils.core import setup
-from glob import glob
 setup(
  name="Bass4Py",
  version="1.0",
  author="Satoprogs",
  author_email="software@satoprogs.de",
  url="http://www.satoprogs.de/",
- package_dir={"Bass4Py":"src/Bass4Py"},
- packages=["Bass4Py"],
- data_files=[("lib", glob('src/Bass4Py/lib/*.dll'))])
+ package_dir={"Bass4Py":"Bass4Py"},
+ packages=["Bass4Py"],)
