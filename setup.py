8bb6c3d1f7a1c2231a32c4f4026a101d966a0957
Timtam <software@satoprogs.de>
2015-01-21 18:53:03 +0100

    - improved setup script to remove build artifacts and prebuild files - also added automatic argument setting if no ones are specified

--- setup.py
+++ setup.py
@@ -1,9 +1,33 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+from glob import iglob,glob
 import os
 import os.path
+import shutil
+import sys
+try:
+ WindowsError
+except:
+ WindowsError=None
+if len(sys.argv)==1:
+ sys.argv.append('build_ext')
+ sys.argv.append('--inplace')
 cd=os.path.dirname(os.path.abspath(__file__))
+exts=['c','dll','pdb','pyd','so']
+print 'Removing old files'
+for ext in exts:
+ ifiles=iglob(os.path.join(cd,'Bass4Py','*.%s'%ext))
+ for file in ifiles:
+  try:
+   os.remove(file)
+  except (IOError,OSError,WindowsError):
+   pass
+try:
+ os.remove(glob(os.path.join(cd,'*.pdb'))[0])
+except:
+ pass
+shutil.rmtree(os.path.join(cd,'cython_debug'),True)
 setup(
  name="Bass4Py",
  version="1.0",
@@ -16,6 +40,11 @@ setup(
   libraries=["bass"],
   library_dirs=[cd],
   include_dirs=[cd],
-  language="c"
+  language="c",
+#  extra_compile_args=['-Zi','/Od'],
+#  extra_link_args=['-debug']
  )
 ))
+print "Removing build artifacts"
+shutil.rmtree(os.path.join(cd,'build'),True)
+print "Finished Bass4Py build process"
