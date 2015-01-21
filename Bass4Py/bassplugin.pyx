ceb70e43f3e370343ebaac76474f7c837e416bac
Timtam <software@satoprogs.de>
2015-01-21 18:53:57 +0100

    - fixed bassplugin warnings related to latest bass wrapper changes

--- Bass4Py/bassplugin.pyx
+++ Bass4Py/bassplugin.pyx
@@ -8,8 +8,8 @@ cdef class BASSPLUGIN:
  cpdef __Evaluate(BASSPLUGIN self):
   cdef bass.DWORD error=bass.BASS_ErrorGetCode()
   if error!=bass.BASS_OK: raise BassError(error)
- cdef bass.BASS_PLUGININFO* __getinfo(BASSPLUGIN self):
-  cdef bass.BASS_PLUGININFO *info=bass.BASS_PluginGetInfo(self.__plugin)
+ cdef const bass.BASS_PLUGININFO* __getinfo(BASSPLUGIN self):
+  cdef const bass.BASS_PLUGININFO *info=bass.BASS_PluginGetInfo(self.__plugin)
   return info
  cpdef Free(BASSPLUGIN self):
   cdef bint res=bass.BASS_PluginFree(self.__plugin)
@@ -17,16 +17,16 @@ cdef class BASSPLUGIN:
   return res
  property Version:
   def __get__(BASSPLUGIN self):
-   cdef bass.BASS_PLUGININFO* info=self.__getinfo()
+   cdef const bass.BASS_PLUGININFO* info=self.__getinfo()
    self.__Evaluate()
    return BASSVERSION(info.version)
  property Formats:
   def __get__(BASSPLUGIN self):
    cdef int i
-   cdef bass.BASS_PLUGININFO *info=self.__getinfo()
+   cdef const bass.BASS_PLUGININFO *info=self.__getinfo()
    formats=[]
    self.__Evaluate()
-   for i in range(info.formatc):
+   for i in range(<int>info.formatc):
     format={'Type':info.formats[i].ctype,'Name':info.formats[i].name,'Extensions':info.formats[i].exts}
     formats.append(format)
    return formats
