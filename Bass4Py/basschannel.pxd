f3e9b47365478b6b9ea89c45843e00de1618805c
Timtam <software@satoprogs.de>
2015-09-16 15:18:03 +0200

    - added channel flags into channel objects.inv

--- Bass4Py/basschannel.pxd
+++ Bass4Py/basschannel.pxd
@@ -4,6 +4,8 @@ cdef class BASSCHANNEL:
  cdef BASS_CHANNELINFO __getinfo(BASSCHANNEL self)
  cpdef __getattribute(BASSCHANNEL self,DWORD attrib)
  cpdef __setattribute(BASSCHANNEL self,DWORD attrib,float value)
+ cdef DWORD __getflags(BASSCHANNEL self)
+ cpdef __setflags(BASSCHANNEL self,DWORD flag,bint switch)
  cpdef Pause(BASSCHANNEL self)
  cpdef Play(BASSCHANNEL self,bint restart)
  cpdef Stop(BASSCHANNEL self)
--- Bass4Py/basschannel.pyx
+++ Bass4Py/basschannel.pyx
@@ -11,6 +11,14 @@ cdef class BASSCHANNEL:
   cdef bint res
   res=bass.BASS_ChannelGetInfo(self.__channel,&info)
   return info
+ cdef DWORD __getflags(BASSCHANNEL self):
+  return bass.BASS_ChannelFlags(self.__channel,0,0)
+ cpdef __setflags(BASSCHANNEL self,DWORD flag,bint switch):
+  if switch:
+   bass.BASS_ChannelFlags(self.__channel,flag,flag)
+  else:
+   bass.BASS_ChannelFlags(self.__channel,0,flag)
+  bass.__Evaluate()
  cpdef __getattribute(BASSCHANNEL self,DWORD attrib):
   cdef float value
   cdef bint res
@@ -206,3 +214,73 @@ cdef class BASSCHANNEL:
    return self.__getattribute(bass.BASS_ATTRIB_VOL)
   def __set__(BASSCHANNEL self,float value):
    self.__setattribute(bass.BASS_ATTRIB_VOL,value)
+ property Loop:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_SAMPLE_LOOP==bass.BASS_SAMPLE_LOOP
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_SAMPLE_LOOP,switch)
+ property AutoFree:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_STREAM_AUTOFREE==bass.BASS_STREAM_AUTOFREE
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_STREAM_AUTOFREE,switch)
+ property RestrictDownload:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_STREAM_RESTRATE==bass.BASS_STREAM_RESTRATE
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_STREAM_RESTRATE,switch)
+ property InterpolationNone:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_NONINTER==bass.BASS_MUSIC_NONINTER
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_NONINTER,switch)
+ property InterpolationSinc:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_SINCINTER==bass.BASS_MUSIC_SINCINTER
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_SINCINTER,switch)
+ property RampNormal:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_RAMP==bass.BASS_MUSIC_RAMP
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_RAMP,switch)
+ property RampSensitive:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_RAMPS==bass.BASS_MUSIC_RAMPS
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_RAMPS,switch)
+ property Surround:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_SURROUND==bass.BASS_MUSIC_SURROUND
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_SURROUND,switch)
+ property Surround2:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_SURROUND2==bass.BASS_MUSIC_SURROUND2
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_SURROUND2,switch)
+ property ModFT2:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_FT2MOD==bass.BASS_MUSIC_FT2MOD
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_FT2MOD,switch)
+ property ModPT1:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_PT1MOD==bass.BASS_MUSIC_PT1MOD
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_PT1MOD,switch)
+ property StopSeeking:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_POSRESET==bass.BASS_MUSIC_POSRESET
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_POSRESET,switch)
+ property StopAllSeeking:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_POSRESETEX==bass.BASS_MUSIC_POSRESETEX
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_POSRESETEX,switch)
+ property StopBackward:
+  def __get__(BASSCHANNEL self):
+   return self.__getflags()&bass.BASS_MUSIC_STOPBACK==bass.BASS_MUSIC_STOPBACK
+  def __set__(BASSCHANNEL self,bint switch):
+   self.__setflags(bass.BASS_MUSIC_STOPBACK,switch)
