3a29bd1ece881a5322b0fced59f04dae4811e681
Toni Barth <software@satoprogs.de>
2019-12-11 22:12:49 +0100

    CI script fix

--- appveyor/build_script.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-IF %SDIST_ONLY% == 1 (
-  %PYTHON%\\python.exe setup.py sdist
-) ELSE (
-  %PYTHON%\\python.exe setup.py build_ext --inplace
-  %PYTHON%\\python.exe setup.py bdist_wheel
-)
