7f93e3f9d81076d3deea4b484bf1a25e0233a1ad
Toni Barth <software@satoprogs.de>
2022-07-14 19:56:38 +0200

    remove a test for output device enumerator because the default device isn't always the default device, it might just be the No sound device without the default flag

--- tests/bass/test_output_device_enumerator.py
+++ tests/bass/test_output_device_enumerator.py
@@ -7,10 +7,6 @@ class TestOutputDeviceEnumerator:
   
     assert bass.output_devices[0].name == 'No sound'
 
-  def test_default_device_is_default_device(self, bass):
-  
-    assert bass.output_devices.default.default is True
-
   def test_negative_index_returns_default_device(self, bass):
   
     assert bass.output_devices.default == bass.output_devices[-1]
